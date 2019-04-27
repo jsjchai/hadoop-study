1a26468993b1d78b0b59ec47b288e649fb577721
jsjchai <jsjchai@163.com>
2019-04-27 21:06:55 +0800

    通过URL获取hdfs文件

---
+++ hello-hadoop/src/main/java/com/jsjchai/hadoop/HelloHdfs.java
@@ -0,0 +1,34 @@
+package com.jsjchai.hadoop;
+
+
+import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * @author jsjchai.
+ */
+public class HelloHdfs {
+
+
+    public static void main(String[] args) {
+
+        InputStream in = null;
+        try {
+            /*设置URL支持hdfs协议*/
+            URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+
+            URL url = new URL("hdfs://master:9000/README.txt");
+            in = url.openStream();
+            IOUtils.copy(in, System.out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            IOUtils.closeQuietly(in);
+        }
+
+    }
+}
