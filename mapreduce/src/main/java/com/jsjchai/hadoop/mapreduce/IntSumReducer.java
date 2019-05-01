399a136cb23063b7a268b4e7e20e2257abca8dc4
jsjchai <jsjchai@163.com>
2019-05-01 11:13:32 +0800

    word count mapreduce

---
+++ mapreduce/src/main/java/com/jsjchai/hadoop/mapreduce/IntSumReducer.java
@@ -0,0 +1,24 @@
+package com.jsjchai.hadoop.mapreduce;
+
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/**
+ * @author jsjchai
+ * @date 2019-05-01 11:07
+ **/
+public class IntSumReducer  extends Reducer<Text, IntWritable, Text, IntWritable> {
+    IntWritable result = new IntWritable();
+
+    @Override
+    public void reduce(Text	key, Iterable<IntWritable> values, Context context) throws IOException,InterruptedException {
+        int sum = 0;
+        for(IntWritable val:values) {
+            sum += val.get();
+        }
+        result.set(sum);
+        context.write(key,result);
+    }
+}
---
+++ mapreduce/src/main/java/com/jsjchai/hadoop/mapreduce/WordMapper.java
@@ -0,0 +1,27 @@
+package com.jsjchai.hadoop.mapreduce;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+/**
+ * @author tianhe
+ * @date 2019-04-25 16:06
+ **/
+public class WordMapper extends Mapper<Object, Text, Text, IntWritable> {
+    private IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+
+    @Override
+    public void map(Object key, Text value, Context context)
+        throws IOException, InterruptedException {
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        while (itr.hasMoreTokens()) {
+            word.set(itr.nextToken());
+            context.write(word, one);
+        }
+    }
+}
